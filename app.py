bfd1b81989b0b99588e904dc5e6a785915abf2b8
Ujeb Pathan <ujeb.pathan@hops.healthcare>
2026-02-18 12:57:20 +0530

    Cicd done

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.end_headers()
-        self.wfile.write(b"Hello,we have added another 3nd features")
+        self.wfile.write(b"Hello,Welcome here")
 
 HTTPServer(('', 3000), handler).serve_forever()
