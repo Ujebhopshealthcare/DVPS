a0d0585a1c11bba99eaa78702be4ba9a4190b3f2
Ujeb Pathan <ujeb.pathan@hops.healthcare>
2026-02-18 14:38:14 +0530

    Crossed half road

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.end_headers()
-        self.wfile.write(b"Hello,Welcome Advance stage")
+        self.wfile.write(b"Hello,You have covered 50to60 stage")
 
 HTTPServer(('', 3000), handler).serve_forever()
